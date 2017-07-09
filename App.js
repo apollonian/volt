1badebe1333a8800ca9d694343e5fd05b8324568
Abhishek Warokar <abhishek11@live.in>
2017-07-09 22:34:39 +0530

    :fire: fiat lux crna

--- App.js
+++ App.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading } from 'expo';
-import { FontAwesome } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
+import { StyleSheet, Text, View } from 'react-native';
 
-import cacheAssetsAsync from './utilities/cacheAssetsAsync';
-
-export default class AppContainer extends React.Component {
-  state = {
-    appIsReady: false,
-  };
-
-  componentWillMount() {
-    this._loadAssetsAsync();
-  }
-
-  async _loadAssetsAsync() {
-    try {
-      await cacheAssetsAsync({
-        images: [require('./assets/images/expo-wordmark.png')],
-        fonts: [
-          FontAwesome.font,
-          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-          { 'pathway-gothic-one': require('./assets/fonts/PathwayGothicOne-Regular.ttf') },
-        ],
-      });
-    } catch (e) {
-      console.warn(
-        'There was an error caching assets (see: main.js), perhaps due to a ' +
-          'network timeout, so we skipped caching. Reload the app to try again.'
-      );
-      console.log(e.message);
-    } finally {
-      this.setState({ appIsReady: true });
-    }
-  }
-
+export default class App extends React.Component {
   render() {
-    if (this.state.appIsReady) {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' &&
-            <View style={styles.statusBarUnderlay} />}
-          <RootNavigation />
-        </View>
-      );
-    } else {
-      return <AppLoading />;
-    }
+    return (
+      <View style={styles.container}>
+        <Text>Opened up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
   }
 }
 
@@ -56,9 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  statusBarUnderlay: {
-    height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
