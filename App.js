7e428e0e86d619b947e0aa564cce8076b020e12b
Abhishek Warokar <abhishek11@live.in>
2017-07-15 11:39:57 +0530

    :art: active icon tinit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavigationComponent } from 'react-native-material-bottom-navigation';
 import { TabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Events } from './screens/Events';
 import { Info } from './screens/Info';
@@ -29,7 +30,7 @@ const AppNavbar = TabNavigator(
     animationEnabled: false,
     tabBarOptions: {
       bottomNavigationOptions: {
-        labelColor: '#727272',
+        labelColor: '#212121',
         rippleColor: '#FFF',
         backgroundColor: '#FFF',
         shifting: true,
@@ -37,6 +38,20 @@ const AppNavbar = TabNavigator(
           height: 56,
           borderTopWidth: 0.5,
           borderTopColor: '#EEEEEE',
+        },
+        tabs: {
+          Home: {
+            activeIcon: <Icon size={24} name="home" color="#212121" />,
+          },
+          Events: {
+            activeIcon: <Icon size={24} name="event" color="#212121" />,
+          },
+          Info: {
+            activeIcon: <Icon size={24} name="info" color="#212121" />,
+          },
+          MapView: {
+            activeIcon: <Icon size={24} name="map" color="#212121" />,
+          },
         },
       },
     },
