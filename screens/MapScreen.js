e484fbb13c44949153b5bea83887338517cb92ae
Abhishek Warokar <abhishek11@live.in>
2017-07-17 18:03:28 +0530

    :earth_asia: MapView update

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 48.860363;
+const LONGITUDE = 2.313201;
+const LATITUDE_DELTA = 0.0052;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export class MapScreen extends Component {
   static navigationOptions = {
@@ -9,25 +16,33 @@ export class MapScreen extends Component {
     tabBarIcon: () => <Icon size={24} name="map" color="#727272" />,
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    };
+  }
+
   render() {
+    const { region } = this.state;
+
     return (
-      <View style={styles.MapViewPort}>
-        <MapView
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={region}
+        pitchEnabled={false}
+        showTraffic={false}
+        showsScale={false}
+        showsBuilding={false}
+        showsPointsOfInterest={false}
+        toolbarEnabled={false}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  MapViewPort: {
-    height: 200,
-    width: 200,
-  },
-});
