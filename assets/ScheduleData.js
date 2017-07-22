26cbc4bb159e2375e999e51a55a55196f43a97d8
Abhishek Warokar <abhishek11@live.in>
2017-07-22 22:51:22 +0530

    :pencil2: schedule data

---
+++ assets/ScheduleData.js
@@ -0,0 +1,44 @@
+export const ScheduleData = [
+  {
+    scheduleID: 0,
+    scheduleTitle: 'Practise I',
+    scheduleStart: '08:00',
+    scheduleEnd: '08:45',
+  },
+  {
+    scheduleID: 1,
+    scheduleTitle: 'Qualifying Group Lottery',
+    scheduleStart: '10:00',
+    scheduleEnd: '10:20',
+  },
+  {
+    scheduleID: 2,
+    scheduleTitle: 'Practise II',
+    scheduleStart: '10:30',
+    scheduleEnd: '11:00',
+  },
+  {
+    scheduleID: 3,
+    scheduleTitle: 'Qualifying',
+    scheduleStart: '12:00',
+    scheduleEnd: '12:45',
+  },
+  {
+    scheduleID: 4,
+    scheduleTitle: 'Super Pole',
+    scheduleStart: '12:45',
+    scheduleEnd: '13:00',
+  },
+  {
+    scheduleID: 5,
+    scheduleTitle: 'Mexico City ePrix',
+    scheduleStart: '16:00',
+    scheduleEnd: '17:00',
+  },
+  {
+    scheduleID: 6,
+    scheduleTitle: 'Podium Celebration',
+    scheduleStart: '17:00',
+    scheduleEnd: '17:20',
+  },
+];
